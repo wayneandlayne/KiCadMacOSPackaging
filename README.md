68b8aa39504fb1ef0c1af21dce74e38b45fcc1fb
Adam Wolf <adamwolf@feelslikeburning.com>
2014-10-29 08:14:10 -0500

    More packaging updates.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This is currently only for my testing.
+This is a set of scripts that Wayne and Layne, LLC uses to provide the nightly and periodic Mac OS X releases of KiCad.
+
+They are meant for our Jenkins cluster.
--- package.sh
+++ package.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -x
 
 FINAL_DMG=kicad.dmg
+NOW=`date +%Y%d%m-%H%M%S`
+KICAD_REVNO=0
 
 if [ "$#" -eq 1 ]; then
-  ARG1_NUMBERS=`echo "$1" | grep '^[0-9]*$'`
-  if [ -z "$ARG1_NUMBERS" ]; then
-    echo "First argument represents bzr revno, and must be completely numeric."
+  KICAD_REVNO=`echo "$1" | grep '^[0-9]*$'`
+  if [ -z "$KICAD_REVNO" ]; then
+    echo "First argument represents KiCad bzr revno, and must be completely numeric."
     exit 1
   else
-    FINAL_DMG=kicad-r$ARG1_NUMBERS.`date +%Y%d%m-%H%M%S`.dmg
+    FINAL_DMG=kicad-r$KICAD_REVNO.$NOW.dmg
   fi
+else
+  echo "First argument (KiCad bzr revno) missing."
+  exit 1
 fi
 
 KICAD_APPS=./bin
@@ -20,7 +25,7 @@ NEW_DMG=kicad.uncompressed.dmg
 MOUNTPOINT=mnt
 
 if [ ! -d $KICAD_APPS ]; then
-   echo "Kicad apps directory doesn't appear to exist."
+   echo "KiCad apps directory doesn't appear to exist."
    exit 1
 fi
 
@@ -40,12 +45,26 @@ rm -r $MOUNTPOINT/Kicad
 mkdir -p $MOUNTPOINT/Kicad
 cp -r ../$KICAD_APPS/* $MOUNTPOINT/Kicad/.
 cp README.template $MOUNTPOINT/README
+cp conf/build.log $MOUNTPOINT
 
 #Update README
 echo "" >> $MOUNTPOINT/README
 echo "Build details" >> $MOUNTPOINT/README
 echo "=============" >> $MOUNTPOINT/README
-echo "Packaged on `date`" >> $MOUNTPOINT/README
+echo "KiCad revision: r$KICAD_REVNO" >> $MOUNTPOINT/README
+echo "Packaged on $NOW" >> $MOUNTPOINT/README
+
+if bzr revno; then
+    echo "Packaging script revision: r`bzr revno`" >> $MOUNTPOINT_README
+fi
+
+if [ -f conf/build_revno ]; then
+    echo "Build script revision: r`cat conf/build_revno`"
+fi
+
+if [ -f conf/cmake_settings ]; then 
+    echo "CMake Settings: `cat conf/cmake_settings`" >> $MOUNTPOINT_README
+fi
 
 hdiutil detach $MOUNTPOINT
 rm -r $MOUNTPOINT
