70356f8c461bebd2b38336270449b3500aef5562
Adam Wolf <adamwolf@feelslikeburning.com>
2014-10-29 09:18:19 -0500

    *sigh*

--- compile.sh
+++ compile.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
 NUM_OF_CORES=3
-CMAKE_SETTINGS="-DCMAKE_C_COMPILER=`which clang` \
-		-DCMAKE_CXX_COMPILER=`which clang++` \
-		-DwxWidgets_CONFIG_EXECUTABLE=../wx-bin/bin/wx-config \
-		-DKICAD_SCRIPTING=OFF \
-		-DKICAD_SCRIPTING_MODULES=OFF \
-		-DKICAD_SCRIPTING_WXPYTHON=OFF \
-		-DCMAKE_INSTALL_PREFIX=../bin \
-		-DCMAKE_BUILD_TYPE=Release"
+CMAKE_SETTINGS="-DCMAKE_C_COMPILER=`which clang` -DCMAKE_CXX_COMPILER=`which clang++` -DwxWidgets_CONFIG_EXECUTABLE=../wx-bin/bin/wx-config -DKICAD_SCRIPTING=OFF -DKICAD_SCRIPTING_MODULES=OFF -DKICAD_SCRIPTING_WXPYTHON=OFF -DCMAKE_INSTALL_PREFIX=../bin -DCMAKE_BUILD_TYPE=Release"
 
 run_cmake() {
 	mkdir -p build
 	cd build
-	cmake ../kicad "$CMAKE_SETTINGS"
+	cmake ../kicad $CMAKE_SETTINGS
 	cd -
 }
 
