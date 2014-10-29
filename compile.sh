29e9883b7344ec705532b7005c7a65c0a4a905d1
Adam Wolf <adamwolf@feelslikeburning.com>
2014-10-29 09:16:14 -0500

    *sigh*

--- compile.sh
+++ compile.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 NUM_OF_CORES=3
-CMAKE_SETTINGS='-DCMAKE_C_COMPILER=`which clang` \
+CMAKE_SETTINGS="-DCMAKE_C_COMPILER=`which clang` \
 		-DCMAKE_CXX_COMPILER=`which clang++` \
 		-DwxWidgets_CONFIG_EXECUTABLE=../wx-bin/bin/wx-config \
 		-DKICAD_SCRIPTING=OFF \
 		-DKICAD_SCRIPTING_MODULES=OFF \
 		-DKICAD_SCRIPTING_WXPYTHON=OFF \
 		-DCMAKE_INSTALL_PREFIX=../bin \
-		-DCMAKE_BUILD_TYPE=Release'
+		-DCMAKE_BUILD_TYPE=Release"
 
 run_cmake() {
 	mkdir -p build
